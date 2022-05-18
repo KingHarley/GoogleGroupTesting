bc582d200c0db91c2cb484dbac138a0c404fce40
Charles Weiss <cweiss1995@gmail.com>
2022-05-18 22:23:03 +0800

    Setting up connection to test project.
    This establishes the connection to the test project. The api calls are being registered on the test project so connection looks to be establishing correctly. The input seems incorrect and needs to be investigated.

--- GoogleGroupTesting/Program.cs
+++ GoogleGroupTesting/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Admin.Directory.directory_v1;
+using Google.Apis.Admin.Directory.directory_v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace AdminSDKDirectoryQuickstart
+{
+    class Program
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/admin-directory_v1-dotnet-quickstart.json
+        static string[] Scopes = { DirectoryService.Scope.AdminDirectoryUserReadonly };
+        static string ApplicationName = "Directory API .NET Quickstart";
+        const string Google_Test_Token_Env_Var = "Google_Test_Token";
+
+        static void Main(string[] args)
+        {
+            
+            var token_file = Environment.GetEnvironmentVariable(Google_Test_Token_Env_Var);
+            if (token_file == null)
+                throw new System.Exception($"The environment variable: {Google_Test_Token_Env_Var} is not set");
+
+            var credential = GoogleCredential.FromFile(token_file).CreateScoped(Scopes).CreateWithHttpClientFactory(null);
+
+            // Create Directory API service.
+            var service = new DirectoryService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            UsersResource.ListRequest request = service.Users.List();
+            request.Customer = "my_customer";
+            request.MaxResults = 10;
+            request.OrderBy = UsersResource.ListRequest.OrderByEnum.Email;
+
+            // List users.
+            IEnumerable<User> users = request.Execute().UsersValue;
+            Console.WriteLine("Users:");
+            if (users.Any())
+            {
+                foreach (var userItem in users)
+                {
+                    Console.WriteLine("{0} ({1})", userItem.PrimaryEmail,
+                        userItem.Name.FullName);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No users found.");
+            }
+            Console.Read();
+        }
+    }
+}
